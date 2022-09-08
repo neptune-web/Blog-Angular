0e3a92ab8ff400020d3ea8f268981cfffa553ded
topdev0528 <top.dev0528@gmail.com>
2022-09-08 21:12:13 +0500

    added backend

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authController = require('../controller/authController');
 const router = express.Router();
 
+
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 
